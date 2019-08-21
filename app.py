d7485e7891fb7c198df4acde3487b5147adc9f3f
stephenau93 <stephenau93@gmail.com>
2019-08-21 15:23:03 -0700

    added dummy data, added user list

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, request, render_template, redirect
-from models import db, connect_db
+from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -9,20 +9,27 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+connect_db(app)
+
 @app.route("/")
 def show_form():
     """display add user form"""
+    users = User.query.all()
+    return render_template('main.html', users=users)
 
-    return render_template('base.html')
-
-@app.route("/user")
+@app.route("/users")
 def show_user():
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    image_url = request.form['image_url']
+    """Render user-form"""
+    return render_template('user-form.html')
 
-    user = User(first_name='first_name', last_name='last_name', image_url='image_url')
-    db.session.add(user)
-    db.commit()
 
-    return 
+# @app.route('/')
+    # first_name = request.form['first_name']
+    # last_name = request.form['last_name']
+    # image_url = request.form['image_url']
+
+    # user = User(first_name='first_name', last_name='last_name', image_url='image_url')
+    # db.session.add(user)
+    # db.commit()
+
+    # return None
