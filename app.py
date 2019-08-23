61c0c6490ce8b9cce40798df12649622cf2196f0
Kimberly Johnson <kajohnson3140@gmail.com>
2019-08-22 18:05:03 -0700

    finished part 2

--- app.py
+++ app.py
@@ -42,24 +42,17 @@ def show_user():
     return render_template('user_form.html')
 
 
-@app.route('/users/<int:id>', methods=["POST", "GET"])
+@app.route('/users/<int:id>')
 def show_user_profile(id):
     """displaying user info on user detail page"""
-    detail = User.query.get_or_404(id)
-    user_id = detail.id
-    user_posts = Post.query.filter_by(user_id=user_id).all()
+    user = User.query.get_or_404(id)
 
-    first_name = detail.first_name
-    last_name = detail.last_name
-    image_url = detail.image_url
+    first_name = user.first_name
+    last_name = user.last_name
+    image_url = user.image_url
 
-    return render_template("user_detail.html", user=detail, first_name=first_name, last_name=last_name, image_url=image_url, posts=user_posts)
+    return render_template("user_detail.html", user=user, first_name=first_name, last_name=last_name, image_url=image_url, posts=user.posts)
 
-@app.route('/posts/<int:post_id>')
-def show_post_detail(post_id):
-    """Show post id"""
-    cur_post = Post.query.get_or_404(post_id)
-    return render_template("post-detail.html", post=cur_post)
 
 
 @app.route("/users/<int:id>/delete", methods=["POST"])
@@ -109,3 +102,34 @@ def post_form(user_id):
         return redirect(f'/users/{user.id}')
 
     return render_template('post-form.html', user_id=user_id)
+
+@app.route('/posts/<int:post_id>', methods=["GET"])
+def show_post_detail(post_id):
+    """Show post id"""
+    cur_post = Post.query.get_or_404(post_id)
+    return render_template("post-detail.html", post=cur_post, user=cur_post.user)
+
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def delete_post(post_id):
+    delete_post = Post.query.get_or_404(post_id)
+    db.session.delete(delete_post)
+    db.session.commit()
+
+    return redirect(f'/users/{user_id}')
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST", "GET"])
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    if request.method == "POST":
+        title = request.form['title']
+        content = request.form['content']
+
+        post.title = title
+        post.content = content
+        db.session.add(post)
+        db.session.commit()
+        return redirect(f'/posts/{post_id}')
+
+    return render_template('edit_post.html', post=post)
